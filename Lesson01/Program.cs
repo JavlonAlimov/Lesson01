8ccc6672ebc624b47693f60a2c77f4ef0cb3cc81
Javlon Alimov <javlonalimov1@gmail.com>
2023-06-24 00:17:33 +0500

    FirstUpdate

--- Lesson01/Program.cs
+++ Lesson01/Program.cs
@@ -1,2 +1,10 @@
 ﻿
 Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Bu mening birinchi dasturim!");
+        Console.WriteLine("Hello, World!");
+    }
+}
